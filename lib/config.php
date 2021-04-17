1f4394afc383b41fc0858bb36b6c305a35e22a1b
vkvis <50129080+vkvis@users.noreply.github.com>
2021-04-17 22:17:58 +0530

    Add files via upload

---
+++ lib/config.php
@@ -0,0 +1,19 @@
+<?php
+include('func.php');
+include('igfunc.php');
+date_default_timezone_set('Asia/Kolkata');
+error_reporting(0);
+
+
+
+$countTarget    = '50'; 
+$sleep_1        = '3'; 
+$sleep_2        = '5';
+
+$answerFile		= 'storyAnswer.txt'; 
+$saveFile 		= 'logData.txt'; 
+$cookieFile 	= 'cookieData.txt'; 
+$targetFile 	= 'targetData.txt'; // File target
+$date 			= date("Y-m-d");
+$time 			= date("H:i:s");
+?>
